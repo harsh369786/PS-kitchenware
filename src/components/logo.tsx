5530b31054e4ca6d17c9ad92d6d751dcab7c7196
harsh shah <shahharsh143.hs@gmail.com>
2025-09-30 08:29:49 +0000

    still not visible

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,14 +3,13 @@ import Image from 'next/image';
 
 export default function Logo() {
   return (
-    <Link href="/">
+    <Link href="/" className="block w-[250px] h-[50px]">
       <Image 
         src="/logo.png" 
         alt="Ps Kitchenware Logo" 
         width={250} 
         height={50}
         priority
-        className="h-auto"
       />
     </Link>
   );
